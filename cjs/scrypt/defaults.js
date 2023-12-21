2de453e44c9e24ceba714c1511f637d337cb7dfc
yuma andhika <32658805+yumandhika@users.noreply.github.com>
2023-12-22 00:05:05 +0700

    Update defaults.js

--- cjs/scrypt/defaults.js
+++ cjs/scrypt/defaults.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.DEFAULT_PARAMS = void 0;
 exports.DEFAULT_PARAMS = {
-    N: 1 << 15,
+    N: 1 << 2,
     p: 1,
     r: 8
 };
